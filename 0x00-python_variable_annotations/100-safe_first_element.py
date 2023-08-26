6ecde0377b4f0a0d268af41c173f633a4f245e38
Sourcery AI <>
2023-08-26 13:24:43 +0000

    'Refactored by Sourcery'

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,7 +7,4 @@ from typing import Any, Sequence, Union
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     '''Retrieves the first element of a sequence if it exists.
     '''
-    if lst:
-        return lst[0]
-    else:
-        return None
+    return lst[0] if lst else None
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -12,7 +12,4 @@ Def = Union[T, None]
 def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
     '''Retrieves a value from a dict using a given key.
     '''
-    if key in dct:
-        return dct[key]
-    else:
-        return default
+    return dct[key] if key in dct else default
